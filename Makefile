0b52be53a102e2f3d2d3b4ab5c547806c7eae16a
andydevs <akanshul97@gmail.com>
2019-06-09 16:27:56 +0000

    Removed DEBUG_PARSE

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ TARGET=akash
 SOURCES=$(wildcard *.c)
 OBJECTS=$(patsubst %.c,%.o,$(SOURCES))
 LIBS=-lreadline
-FLAGS=-g -DDEBUG_PARSE
+FLAGS=-g
 
 executable: $(TARGET)
 
--- execute.c
+++ execute.c
@@ -17,31 +17,5 @@
  * @param parse struct representing parsed command
  */
 void execute_parsed_command(struct parse* parse) {
-	if (parse->shell) {
-		printf("Shell command: %s\n", parse->shcmd);
-	}
-	else {
-		if (parse->infile) {
-			printf("Infile: %s\n", parse->infile);
-		}
-		if (parse->outfile) {
-			printf("Outfile: %s\n", parse->outfile);
-		}
-		printf("Background: %i\n", parse->background);
-		if (parse->tasks) {
-			printf("Tasks:\n");
-			struct task_node* taskn;
-			for (taskn = parse->tasks; taskn; taskn = taskn->next) {
-				printf("	Task:\n");
-				printf("		Command: %s\n", taskn->cmd);
-				if (taskn->args) {
-					printf("		Arguments:\n");
-					struct arg_node* argn;
-					for (argn = taskn->args; argn; argn = argn->next) {
-						printf("			%s\n", argn->arg);
-					}
-				}
-			}
-		}
-	}
+
 }
