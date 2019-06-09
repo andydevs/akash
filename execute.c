60ee6296eb3ed419c0b90a422b69ded90499f309
andydevs <akanshul97@gmail.com>
2019-06-09 22:24:47 +0000

    Moved child handling to separate process

--- execute.c
+++ execute.c
@@ -19,6 +19,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/**
+ * Print on DEBUG_EXECUTE
+ */
 void __debug_execute__printf(const char* fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
@@ -28,32 +31,39 @@ void __debug_execute__printf(const char* fmt, ...) {
 	va_end(args);
 }
 
-void execute_task_as_child(struct task_node* task) {
+/**
+ * Handle child process. Execute task command. Print error if error.
+ *
+ * @param task task node to execute
+ */
+void handle_child(struct task_node* task) {
+	__debug_execute__printf("==========EXECUTE CHILD=========\n");
+	char* const args[2] = { task->cmd, NULL };
+	int error = execvp(task->cmd, args);
+	if (error == -1) {
+		printf("ERROR! Failed to execute %s: %s\n", task->cmd, strerror(errno));
+	} 
+	__debug_execute__printf("================================\n");
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * Fork a child process and execute task in it
+ * 
+ * @param task task node to execute
+ */
+void fork_and_execute_task(struct task_node* task) {
 	__debug_execute__printf("CMD: %s\n", task->cmd);
 	int error;
 	char* const args[2] = { task->cmd, NULL };
 	__debug_execute__printf("==========EXECUTE TASK==========\n");
 	pid_t pid = fork();
-	switch (pid) {
-		case -1:
-			// ERROR
-			printf("ERROR: Failed to fork!\n");
-			break;
-		case 0:
-			// CHILD
-			// Execute statement
-			error = execvp(task->cmd, args);
-			if (error == -1) {
-				printf("ERROR! Failed to execute %s: %s\n", task->cmd, strerror(errno));
-			}
-			exit(EXIT_FAILURE);
-			break;
-		default:
-			// PARENT
-			wait(NULL);	
-			break;
+	switch (pid) {	
+		case -1: printf("ERROR: Failed to fork!\n"); break; // ERROR
+		case 0: handle_child(task); break; // CHILD	
+		default: wait(NULL); break; // PARENT
 	}
-	__debug_execute__printf("===============================\n");
+	__debug_execute__printf("================================\n");
 }
 
 /**
@@ -65,7 +75,7 @@ void execute_parsed_command(struct parse* parse) {
 	__debug_execute__printf("================EXECUTE================\n");
 	if (!parse->shell) {
 		struct task_node* task = parse->tasks;
-		execute_task_as_child(task);
+		fork_and_execute_task(task);
 	}
 	__debug_execute__printf("=======================================\n");
 }
